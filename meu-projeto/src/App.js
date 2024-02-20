d8f098013509d919a3af17e2b2aade05397ce2dc
Raul de Souza <rauldesouza.rds2@gmail.com>
2024-02-20 19:27:30 -0300

    mudei a cor da div!!

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,10 +2,15 @@
 import './App.css';
 
 function App() {
+
+  const name = 'Raul'
+
+
   return (
     <div className="App">
      <h1>Olá React!</h1>
      <p>Meu primeiro App</p>
+     <button>Clique aqui</button>
     </div>
   );
 }
